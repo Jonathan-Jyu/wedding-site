87238dc495c7b2b7e7941a11de26cb8af76be73b
Jonathan <js510409@gmail.com>
2026-05-06 22:52:10 +0800

    add kitty

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 const weddingDate = new Date("2027-01-17T18:00:00+08:00");
 
@@ -38,11 +39,19 @@ export default function Home() {
             OUR WEDDING
           </p>
 
-          <h1 className="mb-6 font-serif text-5xl leading-tight text-stone-800 md:text-7xl">
-            Jonathan
-            <span className="mx-3 text-rose-300">&</span>
-            Ramita
-          </h1>
+          <h1 className="relative mb-6 pt-10 font-serif text-4xl leading-tight text-stone-800 sm:text-5xl md:pt-12 md:text-7xl">
+            <Image
+              src="/kitty蝴蝶結_2.png"
+              alt="kitty bow"
+              width={64}
+              height={64}
+              className="absolute left-1/2 top-0 h-10 w-10 -translate-x-1/2 -rotate-6 opacity-90 md:h-14 md:w-14"
+            />
+
+          <span className="block md:inline">Jonathan</span>
+          <span className="mx-2 text-rose-300 md:mx-3">&</span>
+          <span className="block md:inline">Ramita</span>
+        </h1>
 
           <p className="mb-10 text-xl text-stone-600 md:text-2xl">
             2027 / 01 / 17
